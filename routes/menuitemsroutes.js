e055cd14708aa6b11d168a519cebd600b3bc23b6
Arafat Mamia <arafmamia@gmail.com>
2025-03-24 09:01:55 +0300

    readme file added

---
+++ routes/menuitemsroutes.js
@@ -0,0 +1,24 @@
+const {Router} = require('express')
+const router = Router()
+
+// add a menu item
+router.post('/menu', (req,res)=>{
+
+})
+
+// get all menu items
+router.get('/menu', (req,res)=>{
+
+})
+// get menu items by taste
+router.get('/menu/:test', (req,res)=>{
+
+})
+// update a menu item
+router.put('/menu/:id', (req,res)=>{
+
+})
+// delete a menu item
+router.delete('/menu/:id', (req,res)=>{
+
+})
---
+++ routes/personroutes.js
@@ -0,0 +1,24 @@
+const {Router} = require('express')
+const router = Router()
+
+// add a person
+router.post('/person', (req,res)=>{
+
+})
+
+// get all persons
+router.get('/person', (req,res)=>{
+
+})
+// get persons by work type
+router.get('/person/:workType', (req,res)=>{
+
+})
+// update a person
+router.put('/person/:id', (req,res)=>{
+
+})
+// delete a person
+router.delete('/person/:id', (req,res)=>{
+
+})
